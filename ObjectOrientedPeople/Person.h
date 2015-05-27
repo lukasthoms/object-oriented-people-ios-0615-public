aaef7f23884e6e8dfeff3334ba05fdcdd47c055c
lukasthoms <luk@stho.ms>
2015-05-27 10:52:23 -0400

    Method definition errors

---
+++ ObjectOrientedPeople/Person.h
@@ -0,0 +1,25 @@
+//
+//  Person.h
+//  ObjectOrientedPeople
+//
+//  Created by Lukas Thoms on 5/26/15.
+//  Copyright (c) 2015 al-tyus.com. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Person : NSObject
+
+@property (strong, nonatomic) NSNumber *height;
+@property (strong, nonatomic) NSNumber *age;
+@property (strong, nonatomic) NSString *name;
+@property (nonatomic) BOOL isFemale;
+@property (strong, nonatomic) NSMutableArray *friends;
+
+- (NSNumber *)grow;
+- (void)addFriends:(NSArray *)friends;
+- (NSString *)generatePartyList;
+- (BOOL)removeFriend:(Person *)friend;
+
+
+@end
---
+++ ObjectOrientedPeople/Person.m
@@ -0,0 +1,89 @@
+//
+//  Person.m
+//  ObjectOrientedPeople
+//
+//  Created by Lukas Thoms on 5/26/15.
+//  Copyright (c) 2015 al-tyus.com. All rights reserved.
+//
+
+#import "Person.h"
+
+@implementation Person
+
+
+
+- (NSNumber *) grow
+{
+    if (self.isFemale)
+    {
+        if ([self.age integerValue] < 11) {
+            float low_bound = 0;
+            float high_bound = 1;
+            float rndValue = (((float)arc4random()/0x100000000)*(high_bound-low_bound)+low_bound);
+            float newHeightFloat = [self.height floatValue] + rndValue;
+            self.height = [NSNumber numberWithFloat:newHeightFloat];
+            return self.height;
+        }
+        else if ([self.age integerValue] >= 11 && [self.age integerValue] <= 15) {
+            float low_bound = 0.5;
+            float high_bound = 2;
+            float rndValue = (((float)arc4random()/0x100000000)*(high_bound-low_bound)+low_bound);
+            float newHeightFloat = [self.height floatValue] + rndValue;
+            self.height = [NSNumber numberWithFloat:newHeightFloat];
+            return self.height;
+        }
+        else return self.height;
+    
+        
+    } else
+    {   if ([self.age integerValue] < 12) {
+            float low_bound = 0;
+            float high_bound = 1;
+            float rndValue = (((float)arc4random()/0x100000000)*(high_bound-low_bound)+low_bound);
+            float newHeightFloat = [self.height floatValue] + rndValue;
+            self.height = [NSNumber numberWithFloat:newHeightFloat];
+            return self.height;
+        }
+        else if ([self.age integerValue] >= 12 && [self.age integerValue] <= 16) {
+            float low_bound = 0.5;
+            float high_bound = 3.5;
+            float rndValue = (((float)arc4random()/0x100000000)*(high_bound-low_bound)+low_bound);
+            float newHeightFloat = [self.height floatValue] + rndValue;
+            self.height = [NSNumber numberWithFloat:newHeightFloat];
+            return self.height;
+        }
+        else return self.height;
+    
+}
+
+- (void)addFriends:(NSArray *)friends
+    {
+    self.friends = [friends mutableCopy];
+}
+
+- (NSString *)generatePartyList
+    {
+    NSString *party;
+    for (Person *friend in self.friends) {
+        if (party == nil) {
+            party = friend.name;
+        } else {
+            party = [party stringByAppendingFormat:@", %@", friend.name];
+        }
+    }
+    return party;
+}
+
+- (BOOL)removeFriend:(Person *)friend
+    {
+    NSMutableArray *friends = self.friends;
+    for (Person *person in friends) {
+        if ([person isEqual:friend]) {
+            [self.friends removeObject:person];
+            return YES;
+        } else return NO;
+    }
+}
+
+@end
+
